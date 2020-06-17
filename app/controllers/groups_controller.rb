1c5651ba8a4ecb1c1a4d0f655523cf89a4403cda
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-17 10:49:29 +0900

    Midterm Submit Create messages controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,7 @@ class GroupsController < ApplicationController
     def index
         if  user_signed_in?
             @groups = current_user.groups
+
         else
             render user_session_path
         end
@@ -30,7 +31,7 @@ class GroupsController < ApplicationController
     def update
         group = Group.find(params[:id])
         if group.update(group_params)
-            redirect_to root_path, notice: 'グループを更新しました'
+            redirect_to group_messages_path(group), notice: 'グループを更新しました'
         else
             render :edit
         end
